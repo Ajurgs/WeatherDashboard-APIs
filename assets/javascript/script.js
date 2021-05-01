f513025f5471989a6bff4c2a7fc9fac99608962e
Ajurgs <ajjurgs5954@gmail.com>
2021-04-30 22:37:25 -0600

    finished the api call and printing the current weather to the page

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,7 +4,7 @@ const apikey = "678e30c3be2469678a1d2ae3c9c3878e";
 
 const searchBtn = $("#citySearch");
 const prevCities = $("#searchedCities");
-
+const currentWeather = $("#currentWeather");
 const cities = JSON.parse(localStorage.getItem("cities")) || [];
 // event listeners
 
@@ -21,44 +21,7 @@ searchBtn.on("submit", function (event) {
     console.log(searchEntry);
     getWeatherData(searchEntry);
   }
-
-  //current weather function (input)
-  // build the html for the current weather
-
-  //forecast weather info(input)
-  // make a entry to the page for current weather
-  // add city to list of previous cities
-  // get 5 day forcast
 });
-
-function buildCurrentWeather(data) {
-  let temp = `
-    <div class="card">
-    <div class="card-body">
-      <h5 class="card-title">${
-        data.name
-      } <img src="https://openweathermap.org/img/w/${
-    data.weather[0].icon
-  }.png" alt="${data.weather[0].description}"/>  </h5>
-      <p class="card-text" id="temp">Temp: ${data.main.temp}</p>
-      <p class="card-text">Wind: ${data.wind.speed} </p>
-      <p class="card-text">Humidity: ${data.main.temp}</p>
-      <p class="card-text">
-        UV Index:
-        <span class="${
-          data.current.uvi >= 8
-            ? "bg-danger"
-            : data.current.uvi >= 6
-            ? "bg-warning"
-            : data.current.uvi >= 3
-            ? "bg-success"
-            : "bg-info"
-        } text-light px-2 rounded">${data.current.uvi}</span>
-      </p>
-    </div>
-    </div>
-  `;
-}
 
 function addToCitiesList(toAdd) {
   // get list of child objects in the list
@@ -71,11 +34,14 @@ function addToCitiesList(toAdd) {
     }
   }
   // if not in the list add to the list
-  let newLi = $("LI");
+  //   let newLi = $("<li class='list-group-item'></li>").html(
+  //     `<button class = "btn btn-secondary">${toAdd}</button>`
+  //   );
+  //   prevCities.append(newLi);
 }
 
 function getWeatherData(city) {
-  let url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apikey}`;
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=imperial`;
   //
   let fullData;
   // fetch the API Data
@@ -84,23 +50,56 @@ function getWeatherData(city) {
       return response.json();
     })
     .then(function (data) {
+      // get the coords and then get the all in one function
       console.log(data);
-      callAllInOneAPI(data.coord);
+      callAllInOneAPI(data.coord, data.name);
     });
 }
-
-function callAllInOneAPI(coord) {
+// get the all in one api call
+function callAllInOneAPI(coord, name) {
   let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${coord.lat}&lon=${coord.lon}&appid=${apikey}&units=imperial`;
   fetch(url)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      // retun data
       console.log(data);
-      buildCurrentWeather(data);
+      buildCurrentWeather(data, name);
+      addToCitiesList(name);
     });
 }
+// build out the current weather card
+function buildCurrentWeather(data, name) {
+  let temp = `
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">${name}  
+            <img src="https://openweathermap.org/img/w/${
+              data.current.weather[0].icon
+            }.png"
+            alt="${data.current.weather[0].description}"/> 
+        </h5>
+        <p class="card-text" id="temp">Temp: ${data.current.temp} F</p>
+        <p class="card-text">Wind: ${data.current.wind_speed} MPH</p>
+        <p class="card-text">Humidity: ${data.current.humidity}%</p>
+        <p class="card-text">
+          UV Index:
+          <span class="${
+            data.current.uvi >= 8
+              ? "bg-danger"
+              : data.current.uvi >= 6
+              ? "bg-warning"
+              : data.current.uvi >= 3
+              ? "bg-success"
+              : "bg-info"
+          } text-light px-2 rounded">${data.current.uvi}</span>
+        </p>
+      </div>
+    </div>
+    `;
+  currentWeather.html(temp);
+}
+
 // show saved cities
 function showCitiesList() {}
 // save array of cities
